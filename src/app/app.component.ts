9bd817933235679d7738676776e2bc72f1936c77
McCabe <mccabe@wambl.com>
2017-10-12 14:06:54 -0700

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dojo Mail';
+  topbar = ['email',"importance","subject","content"];
+
+  users=[
+    {email:'bill@gates,com',importance:true, subject:'new windows', content : 'windows xi..'},
+    {email:'ada@lovelace.com', importance : true, subject:'Programming',content : 'Enchantress of numbers'},
+    {email : 'john@carmac.com', importance : false, subject : 'updated algo',content : 'new algo for shadow volumes'},
+    {email: 'gabe@newel.com', importance : false, subject : 'HL3', content : 'jk'}
+  ]
 }
